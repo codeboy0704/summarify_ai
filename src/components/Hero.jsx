7a1af8f4160117a8468df1c36b8c222434f2e088
Draigon07 <ian.flores2@utp.ac.pa>
2024-04-03 10:45:19 -0500

    changes on project view

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,10 +21,10 @@ const Hero = () => {
 
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>OpenAI GPT-4</span>
+        <span className='orange_gradient '>Summarify AI</span>
       </h1>
       <h2 className='desc'>
-        Simplify your reading with Summize, an open-source article summarizer
+        Simplify your reading with Summarify, an open-source article summarizer
         that transforms lengthy articles into clear and concise summaries
       </h2>
     </header>
